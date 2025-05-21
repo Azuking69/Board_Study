2007fedf1db89120944dc86559aec84c3f538262
Azuking69 <azuking69@hotmail.co.jp>
2025-05-21 23:51:39 +0900

    25.05.21 count1

---
+++ board_login/list.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <title>계시판 | 리스트</title>
+</head>
+
+<body>
+    <h1>계시판 > 리스트</h1>
+    <table border="1">
+        <tr>
+            <th>번호</th>
+            <th>이름</th>
+            <th>제목</th>
+            <th>작성일</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>김효찬</td>
+            <td><a href="read.php">아이스크림</a></td>
+            <td>2025-05-20 11:50:30</td>
+        </tr>
+    </table>
+
+    <button type="submit"><a href="index.php">글쓰기</a></button>
+</body>
+
+</html>
