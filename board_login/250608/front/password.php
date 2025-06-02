06a8d934cc76b2e0c712da863d2a24bbd352bc11
Azuking69 <azuking69@hotmail.co.jp>
2025-06-02 23:55:40 +0900

    25.06.02 count4

---
+++ board_login/250608/front/password.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <title>게시판 | 비밀번호 확인</title>
+</head>
+
+<body>
+    <h1>게시판 > 비밀번호 확인</h1>
+
+    <!--🧾パスワード確認フォーム-->
+    <form action="update_delete.php" method="post">
+        <input type="hidden" name="id" value="<?php echo $_POST['id']; ?>">
+        <p>비밀번호: <input type="password" name="password" placeholder="비밀번호를 입력하세요"></p><br>
+        <input type="submit" value="확인">
+    </form>
+</body>
+
+</html>
